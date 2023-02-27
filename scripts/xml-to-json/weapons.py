82c1aafd0559eea4882cdd9ed34a5c8af6653138
Jidost <jiridost@gmail.com>
2023-02-27 18:47:29 +0100

    added: convert weapon xml file to json

--- scripts/xml-to-json/weapons.py
+++ scripts/xml-to-json/weapons.py
@@ -1,7 +1,9 @@
 import os
 import json
 import xml.etree.ElementTree as ET
+from typing import Literal
 
+_PREFERRED_ATTRIBUTE = Literal["value", "name"]
 
 def get_nth_level_parent(current_dir: str, n: int) -> str:
     """
@@ -29,7 +31,8 @@ def get_nth_level_parent(current_dir: str, n: int) -> str:
     # Return the absolute path of the resulting directory path
     return current_dir
 
-def create_dict_from_xml_elements(xml_nodes : ET.Element, attribute_key : str, attribute_value: str) -> dict:
+
+def create_dict_from_xml_elements(xml_nodes: ET.Element, attribute_key: str, attribute_value: str) -> dict:
     """
     Extracts the values of the given attribute names from each node in an XML element tree, and returns a dictionary with attribute_key as the keys and attribute_value as the values.
 
@@ -45,41 +48,92 @@ def create_dict_from_xml_elements(xml_nodes : ET.Element, attribute_key : str, a
     dict_data = {}
     for node in xml_nodes:
         try:
-            dict_data[node.attrib[attribute_key]] = node.attrib[attribute_value]
+            dict_data[node.attrib[attribute_key]
+                      ] = node.attrib[attribute_value]
         except:
             pass
     return dict_data
 
-def parse_group_element(elem):
+
+def get_attribute(element: ET.Element, preffered_ : _PREFERRED_ATTRIBUTE = "value"):
+
+    # prefered value, fallback to name
+    if preffered_ == "value":
+        if "value" in element.attrib:
+            return element.attrib['value']
+        elif "name" in element.attrib:
+            return element.attrib['name']
+        else:
+            return "err_unknown_attrib"
+      
+    # prefered name, fallback to value  
+    elif preffered_ == "name":
+        if "name" in element.attrib:
+            return element.attrib['name']
+        elif "value" in element.attrib:
+            return element.attrib['value'] 
+        else:
+            return "err_unknown_attrib"
+    # uknown
+    else:
+        return "err_unknown_attrib"
+
+
+
+def hasChildren(element: ET.Element):
+    for child in element:
+        return True
+    return False
+
+
+def parse_group_element(element: ET.Element):
     """
     Parse a group XML element and return a dictionary.
     """
     result = {}
-    for child in elem:
-        if child.tag == "group":
-            result[child.attrib["name"]] = parse_group_element(child)
-        else:          
-            try:
-                result[child.attrib["name"]] = child.attrib['value']
-            except:
-                pass
+
+    if hasChildren(element):
+        for child in element:
+            if child.tag == "list":
+                # create a list
+                result[child.attrib["name"]] = []
+                # for all children of a list node
+                for item in child:
+                    listItem = {}
+                    listItem[get_attribute(item,"name")] = parse_group_element(item)
+                    result[get_attribute(child,"name")].append(listItem)
+            else:
+                try:
+                    result[get_attribute(child,"name")] = parse_group_element(child)
+                except:
+                    print("parsing error")
+                    pass
+
+    else:
+        # else xml element doesn't have any children, return the value.
+        result = get_attribute(element,"value")
+
     return result
+
+
+
+#######################################################################################################
 
 
 # this script cwd
 script_root_dir = os.path.abspath(os.getcwd())
 # 2 levels up to the repository
-source_xml_dir = os.path.abspath(os.path.join(get_nth_level_parent(script_root_dir,2), 'xml'))
-#pathStr
+source_xml_dir = os.path.abspath(os.path.join(
+    get_nth_level_parent(script_root_dir, 2), 'xml'))
+# pathStr
 weapon_file = "C:/GIT/coh3-data/xml/attrib/instances/weapon/american/small_arms/machine_gun/sub_machine_gun/thompson_ranger_us.xml"
+#weapon_file = "C:/GIT/coh3-data/xml/attrib/instances/ebps/races/american/infantry/assault_engineer_us.xml"
 
 
 # load weapon_file xml
 xmltree = ET.parse(weapon_file)
 xmlroot = xmltree.getroot()
 # get the default weapon variant(not the single player version!)
-default_variant = xmlroot.findall("variant[@name='default']")[0];
+default_variant = xmlroot.findall("variant[@name='default']")[0]
 weapon = parse_group_element(default_variant)
 weapon_json = json.dumps(weapon)
-
-

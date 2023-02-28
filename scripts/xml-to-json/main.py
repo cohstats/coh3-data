bafa0e5dda9306c45ff9b369f20a97e558fbb2c7
Jiri Dostal <rampik01@gmail.com>
2023-02-28 20:53:08 +0100

    added: gitignore for tempexported folder added: CONST_EXPORT_DIR constant for testing

--- scripts/xml-to-json/main.py
+++ scripts/xml-to-json/main.py
@@ -2,6 +2,9 @@ import os
 from scriptUtils import get_nth_level_parent, build_files_dictionary, save_dict_to_json, get_path_as_string, resolve_dict_value_by_path
 from weapons import parse_weapon_xml_data
 
+CONST_EXPORT_DIR = 'tempexported'   # this is gitignored
+CONST_EXPORT_DIR = 'exported'       # this is not gitignored
+
 print('Parsing xml to json...')
 # this script cwd
 script_root_dir = os.path.abspath(os.getcwd())
@@ -14,7 +17,7 @@ result = build_files_dictionary(weapons_src_dir, parse_weapon_xml_data)
 weapons = resolve_dict_value_by_path(result, get_path_as_string(weapons_src_dir))
 
 # export to json
-exported_weapons_dir = os.path.join(script_root_dir, 'exported\weapon')
+exported_weapons_dir = os.path.join(script_root_dir, f'{CONST_EXPORT_DIR}\weapon')
 save_dict_to_json(weapons, exported_weapons_dir, "weapon.json")
 
-print('Parsing done. View this folder for results: \n"'+ get_path_as_string(script_root_dir)+'\exported"')
+print('Parsing done. View this folder for results: \n"'+ get_path_as_string(script_root_dir)+f'\{CONST_EXPORT_DIR}"')

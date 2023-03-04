ea1b97bd27d834c375ae386fda764f04ee1cd9b8
Jiri Dostal <rampik01@gmail.com>
2023-03-04 14:28:59 +0100

    added: upgrade attributes

--- scripts/xml-to-json/main.py
+++ scripts/xml-to-json/main.py
@@ -50,4 +50,12 @@ sbps = resolve_dict_value_by_path(result, get_path_as_string(sbps_src_dir))
 exported_sbps_dir = os.path.join(script_root_dir, f'{CONST_EXPORT_DIR}\sbps')
 save_dict_to_json(sbps, exported_sbps_dir, "sbps.json")
 
+# navigate to sbps XMLs and build a dictionary
+upgrade_src_dir = os.path.abspath(os.path.join(xml_data_dir, 'attrib\instances\\upgrade'))
+result = build_files_dictionary(upgrade_src_dir, parse_weapon_xml_data)
+upgrade = resolve_dict_value_by_path(result, get_path_as_string(upgrade_src_dir))
+# export to json
+exported_upgrade_dir = os.path.join(script_root_dir, f'{CONST_EXPORT_DIR}\\upgrade')
+save_dict_to_json(upgrade, exported_upgrade_dir, "upgrade.json")
+
 print('Parsing done. View this folder for results: \n"'+ get_path_as_string(script_root_dir)+f'\{CONST_EXPORT_DIR}"')

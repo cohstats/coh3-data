e921c862a91118f2804711b568fd5dda7a49b9f7
KingDarBoja <stevenbojato04@gmail.com>
2023-03-16 13:54:31 -0500

    feat: add battlegroups to exported json

--- scripts/xml-to-json/main.py
+++ scripts/xml-to-json/main.py
@@ -58,4 +58,20 @@ upgrade = resolve_dict_value_by_path(result, get_path_as_string(upgrade_src_dir)
 exported_upgrade_dir = os.path.join(script_root_dir, f'{CONST_EXPORT_DIR}')
 save_dict_to_json(upgrade, exported_upgrade_dir, "upgrade.json")
 
+# Use a function to easily spot each xml-to-json execution.
+def exportBattlegroups():
+    print('## Started processing battlegroups....')
+    # Navigate to tech_tree/battlegroup XMLs and build a dictionary. We do not need the campaign ones.
+    battlegroup_src_dir = os.path.abspath(os.path.join(xml_data_dir, 'attrib\instances\\tech_tree\\battlegroup'))
+    result = build_files_dictionary(battlegroup_src_dir, parse_weapon_xml_data)
+    battlegroup = resolve_dict_value_by_path(result, get_path_as_string(battlegroup_src_dir))
+    # export to json
+    exported_battlegroup_dir = os.path.join(script_root_dir, f'{CONST_EXPORT_DIR}')
+    save_dict_to_json(battlegroup, exported_battlegroup_dir, "battlegroup.json")
+    print('## Finished processing battlegroups....')
+
+
+exportBattlegroups()
+
+
 print('Parsing done. View this folder for results: \n"'+ get_path_as_string(script_root_dir)+f'\{CONST_EXPORT_DIR}"')
